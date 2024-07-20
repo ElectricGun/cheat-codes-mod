6df5befa75970b4d5fd26d07a460c47f55571c42
ElectricGun <->
2024-07-20 17:37:13 +0700

    fixed third impact issues

--- scripts/cheats.js
+++ scripts/cheats.js
@@ -333,31 +333,7 @@ let cheatList = [
             unitGroup.each(unit => {
                 counter ++
                 if (unit != playerUnit) {
-                    Timer.schedule(() => {
-
-                        if (unit.dead != true) {
-
-                            let unitPosition = {
-                                x: unit.getX() / 8,
-                                y: unit.getY() / 8
-                            }
-
-
-                            let randomRadius = Math.random() * 5
-                            let randomAngle = Math.random() * Math.PI * 2
-                        
-                            let splashPosition  = {
-                                x: Math.cos(randomAngle) * randomRadius + unitPosition.x,
-                                y: Math.sin(randomAngle) * randomRadius + unitPosition.y
-                            }
-                            
-                            //splatter
-                            functions.splash(splashPosition.x, splashPosition.y, unit.hitSize, unit.hitSize * 5, unit.hitSize * 25, 0.0025, Liquids.slag)
-
-                            unit.kill()
-                        }
-                        
-                }, counter * (0.15))   
+                    unit.apply(effects.orangeFantaCurse, counter * (0.15) + 2)
                 }
             })
         }, 2)  
