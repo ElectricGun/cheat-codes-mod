cab04efa14a0497f0954a493281ec8dd5c7553e3
ElectricGun <->
2024-07-20 17:37:13 +0700

    fixed third impact issues

--- scripts/effects.js
+++ scripts/effects.js
@@ -1,4 +1,5 @@
 const vars = require("cheat-codes/vars")
+const functions = require("cheat-codes/functions")
 
 const quickfox = extend(StatusEffect, "quickfox", {
     speedMultiplier: 4,
@@ -18,7 +19,33 @@ update(unit, time) {
 }
 })
 
+const orangeFantaCurse = extend(StatusEffect, "orangeFantaCurse", {
+  update(unit, time) {
+    if (time <= 1) {
+      let unitPosition = {
+          x: unit.getX() / 8,
+          y: unit.getY() / 8
+      }
+
+
+    let randomRadius = Math.random() * 5
+    let randomAngle = Math.random() * Math.PI * 2
+
+    let splashPosition  = {
+        x: Math.cos(randomAngle) * randomRadius + unitPosition.x,
+        y: Math.sin(randomAngle) * randomRadius + unitPosition.y
+    }
+    
+    //splatter
+    functions.splash(splashPosition.x, splashPosition.y, unit.hitSize, unit.hitSize * 5, unit.hitSize * 25, 0.0025, Liquids.slag)
+
+    unit.kill()
+    }
+  }
+})
+
   module.exports = {
     quickfox: quickfox,
     timestop: timestop,
+    orangeFantaCurse: orangeFantaCurse,
   }
