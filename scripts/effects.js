72d15cf4e28c807fa7ee430c0817961237255fcc
ElectricGun <->
2024-07-20 17:58:11 +0700

    fixed third impact issues

--- scripts/effects.js
+++ scripts/effects.js
@@ -20,25 +20,29 @@ update(unit, time) {
 })
 
 const orangeFantaCurse = extend(StatusEffect, "orangeFantaCurse", {
+  onRemoved(unit) {
+
+  },
   update(unit, time) {
-    if (time <= 1) {
+    print(time)
+    if (time <= 6000) {
       let unitPosition = {
-          x: unit.getX() / 8,
-          y: unit.getY() / 8
+        x: unit.getX() / 8,
+        y: unit.getY() / 8
       }
 
-    let randomRadius = Math.random() * 5
-    let randomAngle = Math.random() * Math.PI * 2
+      let randomRadius = Math.random() * 5
+      let randomAngle = Math.random() * Math.PI * 2
 
-    let splashPosition  = {
-        x: Math.cos(randomAngle) * randomRadius + unitPosition.x,
-        y: Math.sin(randomAngle) * randomRadius + unitPosition.y
-    }
-    
-    //splatter
+      let splashPosition  = {
+          x: Math.cos(randomAngle) * randomRadius + unitPosition.x,
+          y: Math.sin(randomAngle) * randomRadius + unitPosition.y
+      }
 
-    unit.health = 0
-    functions.splash(splashPosition.x, splashPosition.y, unit.hitSize, unit.hitSize * 5, unit.hitSize * 25, 0.0025, Liquids.slag)
+      //splatter
+
+      unit.health = 0
+      functions.splash(splashPosition.x, splashPosition.y, unit.hitSize, unit.hitSize * 5, unit.hitSize * 25, 0.0025, Liquids.slag)
     }
   }
 })
